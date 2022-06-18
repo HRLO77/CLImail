a03e56cb759257dbaf9d5d06fc12f52d43c861c3
HRLO77 <95102676+HRLO77@users.noreply.github.com>
2022-06-17 20:36:48 -0500

    Update __init__.py

--- climail/__init__.py
+++ climail/__init__.py
@@ -10,7 +10,7 @@ user = User('password', 'email')
 
 
 Customized login:
-user = User('password', 'email', server='smtp.outlook.com',
+user = User('password', 'email', server='outlook.com',
             smtp_port=587, imap_port=993)
 
 # Ports for SMTP and IMAP servers can be found at https://www.systoolsgroup.com/imap/.
