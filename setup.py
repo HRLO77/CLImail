00708803fba184b8a7546e9def918aa502c1e343
HRLO77 <shakebmohammad.10@gmail.com>
2022-06-27 16:23:16 -0500

    Fixed yet another typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='CLImail',
-    version='1.8.0',
+    version='1.8.1',
     license='MIT',
     author="HRLO77",
     author_email='shakebmohammad.10@gmail.com',
