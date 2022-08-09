82512dfc4fe38dc1778cf974ec59cb3c7d81729d
HRLO77 <shakebmohammad.10@gmail.com>
2022-08-08 22:12:55 -0400

    added longer description

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import find_packages, setup
 
+setup()
+
 setup(
     name="CLImail",
-    version="1.9.3",
+    version="1.9.4",
     license="MIT",
     author="HRLO77",
     author_email="shakebmohammad.10@gmail.com",
     packages=["CLImail"],
     url="https://github.com/HRLO77/CLImail",
     description="A Command Line Interface email client written in python.",
-    long_description="A Command Line Interface email client written in python.",
+    long_description="A Command Line Interface email client written in python. Internal classes could be used instead of CLI to send emails (see https://github.com/HRLO77/CLImail).",
 )
